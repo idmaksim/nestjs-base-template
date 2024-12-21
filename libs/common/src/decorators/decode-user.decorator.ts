0782756ed327bfd0fdb2d89603f788ed05324a6c
idmaksim <dmaksim@tuta.io>
2024-12-21 16:11:20 +0300

    Add GraphQL support for user decoding and JWT authentication
    - Introduced `DecodeUserGraphql` decorator to extract user information from GraphQL context.
    - Added `JwtAuthGuardGraphql` class to handle JWT authentication in GraphQL requests, overriding the `getRequest` method to access the request object from the GraphQL context.
    - Enhanced the decorators and guards for better integration with NestJS GraphQL, improving overall functionality and usability.

--- libs/common/src/decorators/decode-user.decorator.ts
+++ libs/common/src/decorators/decode-user.decorator.ts
@@ -1,7 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const DecodeUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     return context.switchToHttp().getRequest().user;
+  },
+);
+
+export const DecodeUserGraphql = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
   },
 );
--- libs/common/src/guards/auth.guard.ts
+++ libs/common/src/guards/auth.guard.ts
@@ -1,3 +1,12 @@
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+export class JwtAuthGuardGraphql extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext): Request {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+}
